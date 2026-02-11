437109e231d0d4fff1ed24e1fab61814893319e4
Brew-Freedom <Dawn.Brewer@pursuit.org>
2026-02-10 22:48:15 -0500

    Update sentiment.test.js

--- training-specialist-final/tests/sentiment.test.js
+++ training-specialist-final/tests/sentiment.test.js
@@ -1,1 +1,14 @@
-// Jest test example
+const { predictSentiment, addWord } = require('../script');
+
+test('predict sentiment correctly', () => {
+  positiveWords = ['good'];
+  negativeWords = ['bad'];
+  document.getElementById('userInput').value = 'I feel good';
+  expect(predictSentiment()).toBe('Positive');
+});
+
+test('addWord adds new word', () => {
+  positiveWords = ['good'];
+  addWordTest('awesome', 'positive');
+  expect(positiveWords.includes('awesome')).toBe(true);
+});
